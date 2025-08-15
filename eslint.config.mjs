692bab4bd686ea305be10c56b091da1d02fe5b09
devuser_minseong <znlwmdhkd1@yonsei.ac.kr>
2025-08-15 13:24:44 +0900

    edit eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+
+    },
+  }),
+]
 
 export default eslintConfig;
